56d27d82f3f4565bade4cd3bb27a8e23a7a452f5
itu-itis-ekmekci23 <ekmekci23@itu.edu.tr>
2024-08-30 10:27:24 +0300

    CSS files are created and some modifications are made

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express=require('express');
-const app=express();
-const dotenv=require('dotenv');
-const connectToDB=require('./db');
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const connectToDB = require('./db');
 
 //to use dotenv
 dotenv.config();
@@ -10,21 +10,42 @@ dotenv.config();
 connectToDB();
 
 
-
-
-
 //for setting view engine as ejs
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 //for using static files
 app.use(express.static('public'));
 
-app.get('/',(req,res) => {
-  res.send('hello');
+app.get('/', (req, res) => {
+  res.render('home');
 })
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+app.get('/addpost', (req, res) => {
+  res.render('addpost');
+});
+app.get('/errorpage', (req, res) => {
+  res.render('errorpage');
+});
+app.get('/list', (req, res) => {
+  res.render('list');
+});
+app.get('/list-ilanlar', (req, res) => {
+  res.render('list-ilanlar');
+});
+app.get('/listsearchresult', (req, res) => {
+  res.render('listsearchresult');
+});
+app.get('/profile', (req, res) => {
+  res.render('profile');
+});
+app.get('/register', (req, res) => {
+  res.render('register');
+});
 
-const port=3000;
-app.listen(port,() => {
-  console.log("Server is running on port http://localhost:"+port);
-  
+
+const port = 3000;
+app.listen(port, () => {
+  console.log("Server is running on port http://localhost:" + port);
 })
