7af2e7d0190118b7951c07d696a989660200fdf4
kalayfurkan <maclicente@gmail.com>
2024-08-30 15:51:55 +0300

    routing

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const userSchema=new mongoose.Schema({
 	username:{type:String,required:true,trim:true},
 	password:{type:String,required:true,trim:true},
 	email:{type:String,required:true,trim:true}
-},{collection:'users'});
+});
 
 const User=mongoose.model("User",userSchema);
 
