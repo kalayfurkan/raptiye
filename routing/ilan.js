e426ec41f4e757fbee4e8c33aec89e01ee162c54
kalayfurkan <maclicente@gmail.com>
2024-09-06 02:09:21 +0300

    ilan edit

--- routing/ilan.js
+++ routing/ilan.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Ilan = require('../models/ilanSchema.js');
-const fs = require('fs').promises;
+const fs = require('fs');
 const sharp = require('sharp');
 const path = require('path');
 const allMiddlewares=require('../middlewares.js');
@@ -59,6 +59,80 @@ router.get('/ilan/:ilanid',allMiddlewares.requireAuth,async (req, res) => {
 		
 		res.render('ilandetay',{ilan});
 
+	} catch (error) {
+		res.status(500).send('Bir hata oluştu.');
+	}
+})
+
+
+router.get('/ilan/edit/:ilanid',allMiddlewares.requireAuth,async (req, res) => {
+	try{
+		const ilanId = req.params.ilanid;
+		const ilan = await Ilan.findById(ilanId);
+		
+		res.render('editpost',{ilan,ilanId:ilanId});
+	}
+	catch(error){
+		res.status(500).send('Bir hata oluştu.');
+	}
+})
+
+router.post('/delete-image/:ilanid/:index',allMiddlewares.requireAuth,async (req, res) => {
+	const ilanId = req.params.ilanid;
+	const ilan = await Ilan.findById(ilanId);
+	
+	const index=req.params.index;
+	const imageToDelete=path.join(__dirname,'../public',ilan.images[index]);
+
+	fs.unlinkSync(imageToDelete);
+	await Ilan.updateOne(
+		{ _id: ilanId },
+		{ $pull: { images: ilan.images[index] } } // Görseli diziden çıkarın
+	);
+
+	res.redirect(`/ilan/edit/${ilanId}`);
+
+})
+
+router.post('/editpost/:ilanId',allMiddlewares.requireAuth,async(req, res) => {
+	const ilanId=req.params.ilanId;
+	const ilan= await Ilan.findById(ilanId);
+
+
+		let images = req.files.images;
+
+		if (!Array.isArray(images)) {
+			images = [images];
+		}
+
+		const maxWidth = 600;
+		const quality = 50;
+
+		for (let element of images) {
+			const date = new Date().toISOString().replace(/:/g, '-');
+			const imagePath = path.resolve(__dirname, '../public/img/postimages', date + element.name);
+			ilan.images.push(`/img/postimages/${date + element.name}`);
+
+			await sharp(element.data)
+				.resize(maxWidth)
+				.jpeg({ quality: quality })
+				.toFile(imagePath);
+		}
+
+
+	const updatedData = {
+		title: req.body.title,
+		date:req.body.date,
+		description: req.body.description,
+		price: req.body.price,
+		images: ilan.images,
+	  };
+	try {
+		await Ilan.updateOne(
+			{ _id: ilanId }, 
+			{ $set: updatedData } 
+		  );
+		  res.redirect('/profile'); 
 	} catch (error) {
 		res.status(500).send('Bir hata oluştu.');
 	}
--- routing/profile.js
+++ routing/profile.js
@@ -14,4 +14,5 @@ router.get('/profile',allMiddlewares.requireAuth,async (req, res) => {
 
 
 
+
 module.exports = router;
