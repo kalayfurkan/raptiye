aa5e130224a5cd90eae7a527920d4e52c78807cb
kalayfurkan <maclicente@gmail.com>
2024-09-07 00:51:43 +0300

    fuaağğağat

--- routing/profile.js
+++ routing/profile.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const allMiddlewares=require('../middlewares.js');
 const Ilan = require('../models/ilanSchema.js');
 const User = require('../models/userSchema');
-
+const Job = require('../models/jobSchema');
 
 router.get('/profile',allMiddlewares.requireAuth,async (req, res) => {
 	const myAllPosts=await Ilan.find({owner:req.session.userId});
 	const user=await User.findById(req.session.userId);
-	res.render('profile',{ilanlar:myAllPosts,user});
+	const jobs=await Job.find({owner:req.session.userId});
+	res.render('profile',{ilanlar:myAllPosts,user,jobs});
 });
 
 
