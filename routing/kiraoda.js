2f597655d9fc8afcc37ea9f240963d236dd7993e
kalayfurkan <maclicente@gmail.com>
2024-09-08 14:16:08 +0300

    final kira updates

--- routing/kiraoda.js
+++ routing/kiraoda.js
@@ -63,4 +63,102 @@ router.get('/kiraoda/edit/:kiraodaid',allMiddlewares.requireAuth,async(req,res) 
 
 	res.render('kiraodaedit',{kira,kiraid});
 })
+
+router.post('/kiraoda/edit/:kiraodaid',allMiddlewares.requireAuth,async(req,res) => {
+	const kiraid=req.params.kiraodaid;
+	const kira=await Kiraoda.findById(kiraid);
+
+	let images = req.files?.images;
+
+	if(images){
+		if (!Array.isArray(images)) {
+			images = [images];
+		}
+	
+		const maxWidth = 600;
+		const quality = 50;
+	
+		for (let element of images) {
+			const date = new Date().toISOString().replace(/:/g, '-');
+			const imagePath = path.resolve(__dirname, '../public/img/kiraodaimages', date + element.name);
+			kira.images.push(`/img/kiraodaimages/${date + element.name}`);
+	
+			try {
+				await sharp(element.data)
+					.resize(maxWidth)
+					.jpeg({ quality: quality })
+					.toFile(imagePath);
+			} catch (error) {
+				return res.status(500).send('Resim işlenirken hata oluştu.');
+			}
+		}
+	}
+	
+	const updatedData = {
+		title: req.body.title,
+		searchingFor:req.body.searchingFor,
+		reachYou:req.body.reachYou,
+		description: req.body.description,
+		images: kira.images,
+	};
+
+	try {
+		await Kiraoda.updateOne(
+			{ _id: kiraid },
+			{ $set: updatedData }
+		);
+		res.redirect('/profile');
+	} catch (error) {
+		res.status(500).send('Bir hata oluştu.');
+	}
+})
+
+router.post('/delete-kiraodaimage/:kiraid/:index', allMiddlewares.requireAuth, async (req, res) => {
+	const kiraid = req.params.kiraid;
+	const kira = await Kiraoda.findById(kiraid);
+	
+	const index = req.params.index;
+	const imageToDelete = path.join(__dirname, '../public', kira.images[index]);
+
+	fs.unlinkSync(imageToDelete);
+	await Kiraoda.updateOne(
+		{ _id: kiraid },
+		{ $pull: { images: kira.images[index] } } // Görseli diziden çıkarın
+	);
+
+	res.redirect(`/kiraoda/edit/${kiraid}`);
+
+})
+
+
+router.post('/kiraoda/delete/:kiraodaid',allMiddlewares.requireAuth,async(req, res) => {
+	const kiraodaid = req.params.kiraodaid;
+	const kira = await Kiraoda.findById(kiraodaid);
+
+	if (!kira) {
+		return res.status(404).send('İlan bulunamadı.');
+	}
+
+	if (kira.images.length > 0) {
+		for (const address of kira.images) {
+			const fullImagePath = path.join(__dirname, '../public', address);
+			try {
+				fs.unlinkSync(fullImagePath);
+				console.log(`Image deleted: ${fullImagePath}`);
+			} catch (error) {
+				console.error(`Failed to delete image: ${fullImagePath}`, error);
+			}
+		}
+	}
+
+	try {
+		await Kiraoda.deleteOne({ _id: kiraodaid });
+		res.redirect('/profile');
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Bir hata oluştu.');
+	}
+})
+
+
 module.exports = router;
