1213222ad091266c2c4cfbabbf9955d9a1cf7392
fuatkarakaya <fuatkarakaya00@gmail.com>
2024-09-16 18:19:05 +0300

    message bubbles styles

---
+++ public/js/messages.js
@@ -0,0 +1,6 @@
+const chatMessages = document.getElementById('chat-messages');
+// Function to scroll to the bottom
+function scrollToBottom() {
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+}
+scrollToBottom();
